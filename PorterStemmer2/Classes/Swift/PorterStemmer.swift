19cba8431f17e62f561560da33697de3c136049f
Chris Eidhof <chris@eidhof.nl>
2021-07-31 12:00:11 +0200

    Modernize the code for Swift 5

--- PorterStemmer2/Classes/Swift/PorterStemmer.swift
+++ PorterStemmer2/Classes/Swift/PorterStemmer.swift
@@ -17,43 +17,22 @@ import Darwin
 import libstemmer
 #endif
 
-public enum StemmerLanguage: CustomStringConvertible {
-    
-    case Danish
-    case Dutch
-    case English
-    case Finnish
-    case French
-    case German
-    case Hungarian
-    case Italian
-    case Norwegian
-    case Portuguese
-    case Romanian
-    case Russian
-    case Spanish
-    case Swedish
-    case Turkish
-    
-    public var description: String {
-        switch self {
-        case .Danish: return "danish"
-        case .Dutch: return "dutch"
-        case .English: return "english"
-        case .Finnish: return "finnish"
-        case .French: return "french"
-        case .German: return "german"
-        case .Hungarian: return "hungarian"
-        case .Italian: return "italian"
-        case .Norwegian: return "norwegian"
-        case .Portuguese: return "portuguese"
-        case .Romanian: return "romanian"
-        case .Russian: return "russian"
-        case .Spanish: return "spanish"
-        case .Swedish: return "swedish"
-        case .Turkish: return "turkish"
-        }
-    }
+public enum StemmerLanguage: String {
+    case danish
+    case dutch
+    case english
+    case finnish
+    case french
+    case german
+    case hungarian
+    case italian
+    case norwegian
+    case portuguese
+    case romanian
+    case russian
+    case spanish
+    case swedish
+    case turkish
 }
 
 public class PorterStemmer
@@ -61,7 +40,7 @@ public class PorterStemmer
     internal let stemmer: OpaquePointer
 
     public init?(withLanguage language: StemmerLanguage) {
-        if let stemmer = sb_stemmer_new(language.description, nil) {
+        if let stemmer = sb_stemmer_new(language.rawValue, nil) {
             self.stemmer = stemmer
         }
         else {
